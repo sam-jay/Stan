87bbeeb3d265584ed5d79d43c9f030eeebf35238
bslakter <bslakter@dyn-129-236-239-179.dyn.columbia.edu>
2015-04-06 15:37:34 -0400

    feat(app.js): adding routes

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-(function() {
-  'use strict';
-  var config = require('./config');
-})()
+var config = require('./config');
+	express = require('express');
+	AWS = require('aws-sdk'); 
+
+
+
+var s3 = new AWS.S3(); 
+ s3.createBucket({Bucket: 'myBucket'}, function() {
+  var params = {Bucket: 'myBucket', Key: 'myKey', Body: 'Hello!'};
+  s3.putObject(params, function(err, data) {
+      if (err) 
+        console.log(err)     
+      else       
+      	console.log("Successfully uploaded data to myBucket/myKey");   
+   });
+});
