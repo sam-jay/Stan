6d1146744e3106887ebffcf8989f6286f73ef4de
bslakter <bslakter@dyn-129-236-239-179.dyn.columbia.edu>
2015-04-06 16:45:24 -0400

    feat(app.js): adding routes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var config = require('./config');
-	express = require('express');
+var express = require('express'),
+	DynamoDBModel = require('dynamodb-model'),
 	AWS = require('aws-sdk'); 
 
-
+// 
+/*
 var s3 = new AWS.S3(); 
  s3.createBucket({Bucket: 'myBucket'}, function() {
   var params = {Bucket: 'myBucket', Key: 'myKey', Body: 'Hello!'};
@@ -13,18 +14,35 @@ var s3 = new AWS.S3();
       	console.log("Successfully uploaded data to myBucket/myKey");   
    });
 });
+*/ 
+
 
 var router = express.Router();
 
-router.get('/auth_service/users/:id', controller. )
-router.get('/auth_service/groups/:id', controller. )
-router.get('/auth_service/resources/:id', controller. )
-router.post('/auth_service/users', controller. )
-router.post('/auth_service/groups', controller.)
-router.post('/auth_service/resources', controller. )
-router.put('/auth_service/users/:id', controller. )
-router.put('/auth_service/groups/:id', controller. )
-router.put('/auth_service/resources/:id', controller. )
-router.delete('/auth_service/users/:id', controller. )
-router.delete('/auth_service/groups/:id', controller. )
-router.delete('/auth_service/resources/:id', controller. )
+// tokens
+router.get('/auth_service/tokens', controllers.tokens.getTokens)
+
+// users
+router.get('/auth_service/users/:id', controllers.users.getUser)
+router.post('/auth_service/users', controllers.users.postUser)
+router.put('/auth_service/users/:id', controllers.users.putUser)
+router.delete('/auth_service/users/:id', controllers.users.deleteUser)
+
+// groups
+router.get('/auth_service/groups/:id', controllers.groups.getGroup)
+router.post('/auth_service/groups', controllers.groups.postGroup)
+router.put('/auth_service/groups/:id', controllers.groups.putGroup)
+router.delete('/auth_service/groups/:id', controllers.groups.deleteGroup)
+
+// resources
+router.get('/auth_service/resources/:id', controllers.resources.getResource)
+router.post('/auth_service/resources', controllers.resources.postResource)
+router.put('/auth_service/resources/:id', controllers.resources.putResource)
+router.delete('/auth_service/resources/:id', controllers.resources.deleteResource)
+
+/*
+Access Key ID:
+AKIAIA7NTQBR73JI6VHQ
+Secret Access Key:
+4OdPEcmIOE9vTVcvfwbGX0oLEKFd/EqseDZIM7Eu
+/*
