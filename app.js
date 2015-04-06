f7ba4a4f0f09f2876061536f7e4298b586e2676a
bslakter <bslakter@dyn-129-236-239-179.dyn.columbia.edu>
2015-04-06 16:30:00 -0400

    files(token.js): tokens controller underway

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var config = require('./config');
 	AWS = require('aws-sdk'); 
 
 
-
 var s3 = new AWS.S3(); 
  s3.createBucket({Bucket: 'myBucket'}, function() {
   var params = {Bucket: 'myBucket', Key: 'myKey', Body: 'Hello!'};
@@ -14,3 +13,18 @@ var s3 = new AWS.S3();
       	console.log("Successfully uploaded data to myBucket/myKey");   
    });
 });
+
+var router = express.Router();
+
+router.get('/auth_service/users/:id', controller. )
+router.get('/auth_service/groups/:id', controller. )
+router.get('/auth_service/resources/:id', controller. )
+router.post('/auth_service/users', controller. )
+router.post('/auth_service/groups', controller.)
+router.post('/auth_service/resources', controller. )
+router.put('/auth_service/users/:id', controller. )
+router.put('/auth_service/groups/:id', controller. )
+router.put('/auth_service/resources/:id', controller. )
+router.delete('/auth_service/users/:id', controller. )
+router.delete('/auth_service/groups/:id', controller. )
+router.delete('/auth_service/resources/:id', controller. )
---
+++ controllers/tokens.js
@@ -0,0 +1,34 @@
+var config = require('./config');
+	AWS = require('aws-sdk'); 
+	DynamoDBModel = require('dynamodb-model');
+	users = DyanamoDBModel.Model('User')
+	resources = DyanamoDBModel.Model('Resource')
+	groups = DyanamoDBModel.Model('Group')
+
+
+
+getTokens(function(req,res){
+	if (req.headers['authorization'] !== undefined){
+	  var header=req.headers['authorization']||'',        // get the header
+	      token=header.split(/\s+/).pop()||'',            // and the encoded auth token
+	      auth=new Buffer(token, 'base64').toString(),    // convert from base64
+	      parts=auth.split(/:/),                          // split on colon
+	      username=parts[0],
+	      password=parts[1];
+	  }
+
+	  #look up username and password to get LIST of groups
+	  look up groups to get SET of resources
+	  look up resources to get LIST of tokens
+
+
+	  eventually
+	  /*
+	  	return res.json(200, {
+			tokens: token_list
+	  	})
+	  */
+
+  res.writeHead(200,{'Content-Type':'text/plain'});
+  res.end('username is "'+username+'" and password is "'+password+'"');
+})
